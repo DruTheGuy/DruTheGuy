421a95403994b97371d9d3d9d79ff6e642b72823
Dru <druthedeveloper@gmail.com>
2025-06-29 05:06:52 -0500

    Update celciuscalc.cpp
    added some comments

--- celciuscalc.cpp
+++ celciuscalc.cpp
@@ -9,9 +9,10 @@ int main() {
   std::cout << "Enter the temperature in Fahrenheit: ";
   std::cin >> tempf;
   
-  
+  //calculate the temperature
   tempc = (tempf - 32) / 1.8;
-  
+
+  //output the answer
   std::cout << "The temp is " << tempc << " degrees Celsius.\n";
   
 }
